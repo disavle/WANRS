deb749d892f604a3f219348ce09725aa5bc2f3c5
Dima DIsavle <instadimka@gmail.com>
2022-07-06 20:39:17 +0300

    Добавил Posts API

--- backend/WANRS/api/views.py
+++ backend/WANRS/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from . import serializer
 from django.contrib.auth.models import User
+from .models import Post
 
 
 class UserList(generics.ListAPIView):
@@ -11,3 +12,15 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializer.UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializer.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializer.PostSerializer
