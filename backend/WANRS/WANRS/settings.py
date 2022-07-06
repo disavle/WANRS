b39c7d6a5fb489a346e76e13ce5672e39e3e7fb5
Dima DIsavle <instadimka@gmail.com>
2022-07-06 20:51:41 +0300

    Добавил образ Docker
    А также новый сервис Posts

--- backend/WANRS/WANRS/settings.py
+++ backend/WANRS/WANRS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+)n$b$6@70d3ohwaabklrcx!lmok&cz!9v_8zjchf8*)xtp)t5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
